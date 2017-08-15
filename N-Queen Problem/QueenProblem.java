f783165e66b37437d4ac6778d85d955429963015
Gaurav Dangi <gauravdangi96@gmail.com>
2017-08-15 13:51:25 +0530

    N Queen Problem

---
+++ N-Queen Problem/QueenProblem.java
@@ -0,0 +1,78 @@
+package pkg8.queen.problem;
+import java.util.*;
+
+/*
+ * @author Gaurav
+ */
+public class QueenProblem {
+    
+    private static int N;
+    private LinkedList<Integer> arr = new LinkedList<>();
+    
+    
+    // ---------------- isSafe () --------------------------------------
+    public boolean isSafe(int mat[][],int r, int c){
+        for(int i=0;i<N;i++){
+            if(mat[r][i] == 1) return false;
+            if(mat[i][c] == 1) return false;
+        }
+        for (int i = r,j = c;i >= 0 && j >= 0;i--,j--) {
+		if (mat[i][j] == 1)
+			return false;
+		}
+        for (int i = r,j = c;i<N && j>=0;i++,j--) {
+		if (mat[i][j] == 1)
+			return false;
+		}
+        return true;
+    
+    }
+    
+    
+    public boolean solveNQProblem(int mar[][], int col){   
+        if(col>=N)
+            return true;
+        for(int i=0;i<N;i++){
+            if(isSafe(mar,i,col)){
+                mar[i][col] = 1;
+                if(solveNQProblem(mar,col+1))
+                    return true;
+                mar[i][col] = 0;
+            }
+        }
+        return false;
+    }
+     
+      void printSolution(int board[][])
+    {  
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			if (board[i][j] == 1) {
+                            arr.add(j+1);
+				System.out.print("Q ");
+			} else {
+				System.out.print("_ ");
+			}
+		System.out.println("\n");
+		}
+        for (int i = 0; i < N; i++){
+            System.out.println("row: "+(i+1)+"| Column: "+arr.get(i));
+        
+        }
+        
+    }
+    public static void main(String[] args) {
+        QueenProblem Q = new QueenProblem();
+        Scanner scan = new Scanner(System.in);
+	System.out.println("State the value of N in this program!");
+	N = scan.nextInt();
+	int[][] board = new int[N][N];
+	if (!Q.solveNQProblem(board, 0)) {
+		System.out.println("Solution not found.");
+                System.exit(0);
+	}
+	Q.printSolution(board);
+       
+    }
+    
+}
